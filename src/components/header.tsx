ce96407e4a5b56fb83ef52bef550a7fea499400b
Hesed Anu Afolami <hesedanu@gmail.com>
2025-12-16 14:06:39 +0000

    move this to the far end of the page (_for element <Button>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,13 +6,13 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-4 flex items-center">
+        <div className="flex flex-1 items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <BitcoinIcon className="h-6 w-6 text-primary" />
             <span className="font-bold">STX TipJar</span>
           </Link>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex items-center justify-end space-x-2">
           <ConnectWalletButton />
         </div>
       </div>
