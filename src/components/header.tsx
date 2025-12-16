5d86373e2eaed944954c0f3333504fb3368948f4
Hesed Anu Afolami <hesedanu@gmail.com>
2025-12-16 14:11:18 +0000

    center this navbar ( align it to the middle) (_for element div_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import { ConnectWalletButton } from './connect-wallet-button';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 p-4">
-      <div className="container flex h-14 items-center rounded-2xl border border-white/20 bg-background/50 p-4 shadow-lg backdrop-blur-lg">
+    <header className="sticky top-0 z-50 flex justify-center p-4">
+      <div className="container flex h-14 max-w-4xl items-center rounded-2xl border border-white/20 bg-background/50 p-4 shadow-lg backdrop-blur-lg">
         <div className="flex flex-1 items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <BitcoinIcon className="h-6 w-6 text-primary" />
