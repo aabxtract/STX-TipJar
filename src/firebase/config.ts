e0ba5483d6a71f31c0dfdd72f882aa978d289ba7
Hesed Anu Afolami <hesedanu@gmail.com>
2025-12-18 12:49:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3645772914-61b90",
+  "appId": "1:920727087474:web:51098efb68937b2363d5eb",
+  "apiKey": "AIzaSyC5oRtv45jRVzyAu02UcZ7ccGLTtgixZiw",
+  "authDomain": "studio-3645772914-61b90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "920727087474"
+};
