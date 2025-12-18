bf7c1cbad3ad6bda622e4b6741b769e2b9fb9446
Hesed Anu Afolami <hesedanu@gmail.com>
2025-12-18 08:01:09 +0000

    wireToPostCondition? rewrite the code to this

--- src/lib/stacks.ts
+++ src/lib/stacks.ts
@@ -10,7 +10,7 @@ import {
   cvToJSON,
   PostConditionMode,
   FungibleConditionCode,
-  makeStandardSTXPostCondition,
+  createStandardStxPostCondition,
 } from '@stacks/transactions';
 import { userSession } from '@/lib/wallet-context-provider';
 import { openContractCall } from '@stacks/connect';
@@ -69,7 +69,7 @@ export async function sendTip(tip: { recipient: string, amount: number, message?
 
     // Post-condition: Ensure the sender transfers the exact amount of STX
     const postConditions = [
-        makeStandardSTXPostCondition(
+        createStandardStxPostCondition(
             senderAddress,
             FungibleConditionCode.Equal,
             BigInt(amount * 1000000)
